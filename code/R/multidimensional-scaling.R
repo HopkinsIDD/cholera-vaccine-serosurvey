e3bb13248655e58f15ad51236f8609bcacf0e1fe
Forrest K Jones <46634574+fjones2222@users.noreply.github.com>
2023-03-21 20:17:57 -0400

    figure 2 code

--- code/R/multidimensional-scaling.R
+++ code/R/multidimensional-scaling.R
@@ -22,19 +22,36 @@ generate_MDS <- function(data,regex_string="Ogawa|Inaba|TcpA|CtxB|IgG_O139"){
 
 
 
+# compare_data_wide <- final_df %>%
+#         # only individuals from bandladesh
+#         filter(cohort %in% c("BGD Vaccinee","SMIC/PIC")) %>%
+#         # recent
+#         filter(day>3,day<50) %>%
+#         #only cases and vaccinees
+#         filter(status %in% c("Case","Vaccinee"))%>%
+#         select(sample,id,isotype,antigen_pretty,marker,RAU_value,day,age,status) %>% 
+#         select(-isotype,-antigen_pretty) %>%
+#         # mutate(RAU_value=log10(1/RAU_value)) %>%
+#         spread(marker,RAU_value)
+# mds_obj <- bind_rows(
+#         compare_data_wide %>% 
+#                 generate_MDS() %>% mutate(variables="All"),
+#         compare_data_wide %>% 
+#                 generate_MDS(regex_string="IgG_Ogawa|IgG_Inaba|IgG_CtxB") %>% mutate(variables="Original"),
+#         compare_data_wide %>% 
+#                 generate_MDS(regex_string="CtxB") %>%  mutate(variables="CtxB")
+# )
+
+
 compare_data_wide <- final_df %>%
         # only individuals from bandladesh
-        filter(cohort %in% c("BGD Vaccinee","SMIC/PIC")) %>%
-        # recent
-        filter(day>3,day<200) %>%
+        filter(cohort %in% c("BGD Vaccinee","SMIC/PIC", "HTI Vaccinee")) %>%
         #only cases and vaccinees
         filter(status %in% c("Case","Vaccinee"))%>%
         select(sample,id,isotype,antigen_pretty,marker,RAU_value,day,age,status) %>% 
         select(-isotype,-antigen_pretty) %>%
         # mutate(RAU_value=log10(1/RAU_value)) %>%
         spread(marker,RAU_value)
-
-
 
 
 mds_obj <- bind_rows(
