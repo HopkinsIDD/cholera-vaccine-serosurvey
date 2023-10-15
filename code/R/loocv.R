b67ace97c466df000b79014388b49d4ceef3f59f
Forrest K Jones <46634574+fjones2222@users.noreply.github.com>
2023-10-14 22:36:51 -0400

    update loocv analysis

--- code/R/loocv.R
+++ code/R/loocv.R
@@ -1,3 +1,5 @@
+rm(list=ls())
+
 source("code/R/packages.R")
 source("code/R/utils.R")
 source("code/R/load-data.R")
@@ -12,32 +14,41 @@ var_list <- list(
 )
 
 
-#get fit data
-bangladeshi_fitdata <- final_wide %>%
-        filter(cohort %in% c("BGD Vaccinee" , "SMIC/PIC"))  
-missing <-bangladeshi_fitdata[,var_list$`All Variables`] %>% is.na() %>% rowSums()
-bangladeshi_fitdata <- bangladeshi_fitdata[missing==0,]
+
+#get data sets for the analysis
+data_list <- list(
+        smicpic_fitdata =  final_wide %>%
+                filter(cohort %in% c("SMIC/PIC")),
+        all_fitdata = final_wide %>%
+                filter(cohort %in% c("SMIC/PIC","BGD Vaccinee","HTI Vaccinee")),
+        bgd_fitdata = final_wide %>%
+                filter(cohort %in% c("SMIC/PIC","BGD Vaccinee"))
+)
 
 
 
 #important variables
 tree_param <- 1000
 t <-200
-ids <- unique(bangladeshi_fitdata$id) 
 
 
 loocv_list <- list()
 raw_df <- data.frame()
 
 for(v in 1:length(var_list)){
+        for (q in names(data_list)){
+        
+        base_data <- data_list[[q]]
+        ids <- unique(base_data$id) 
+                
         for(i in ids){
-
+                
                 
                 marks <- var_list[[v]]
                 
-                cat("Window: ",t, "Variables: ",names(var_list)[v],"Individiual: ",i,"\n")        
+                cat("Data: ",q, "Variables: ",names(var_list)[v],"Individiual: ",i,"\n")        
                 
-                fit_data <- bangladeshi_fitdata %>%
+                fit_data <- base_data %>%
                         filter(id!=i) %>%
                         addInfectionWindow(end_window=t) 
                 
@@ -49,10 +60,13 @@ for(v in 1:length(var_list)){
                 
                 for(j in 1:100){
                         
-                        n_ind <-unique(fit_data$id) %>% length() 
-                        
+
                         #define who is in and who is out
-                        outside <-sample(fit_data$id,round(n_ind*0.5),replace=FALSE)
+                        outside <-fit_data %>%
+                                distinct(cohort,id) %>%
+                                group_by(cohort) %>% 
+                                slice_sample(prop=0.5,replace = FALSE) %>%
+                                pull(id)
                         outside_df <- filter(fit_data,id %in% outside)
                         inside_df  <- filter(fit_data,!id %in% outside)%>%
                                 addInfectionWindow(end_window=t)
@@ -78,8 +92,7 @@ for(v in 1:length(var_list)){
                         
                         
                         #find the cutoff for each 
-                        performance <- 
-                                outside_df %>% addInfectionWindow(end_window=t) %>%
+                        performance <-  outside_df %>% addInfectionWindow(end_window=t) %>%
                                 getPerf_ranger(inside_fit,paste0("inf_",t)#,
                                                # spec=train_spec_cutoff
                                 )
@@ -126,7 +139,7 @@ for(v in 1:length(var_list)){
                 #calculate the individual's samples seropositivity
                 # test_data <- vax_wide_RAU %>%
                 #         addInfectionWindow(end_window=t)
-                test_data <- bangladeshi_fitdata %>%
+                test_data <- base_data %>%
                         filter(id==i) %>%
                         addInfectionWindow(end_window=t)
                 
@@ -161,7 +174,8 @@ for(v in 1:length(var_list)){
                                 
                         ) %>%
                         mutate(variables=names(var_list)[v]) %>%
-                        mutate(end_window=as.character(t))
+                        mutate(end_window=as.character(t)) %>%
+                        mutate(base_data_name=q)
                 
                 out_tmp <- list(
                         cutoffs=cut_data,
@@ -169,19 +183,190 @@ for(v in 1:length(var_list)){
                         fit=this_fit,
                         preds=fpr_df
                 )
-
-                loocv_list[[names(var_list)[v]]][[i]] <- out_tmp
+                
+                loocv_list[[names(var_list)[v]]][[q]][[i]] <- out_tmp
                 raw_df <- bind_rows(raw_df,fpr_df)
                 
                 
+                write_rds(raw_df, "data/generated_data/analysis_objects/loocv_df.rds")
                 
                 
                 
-}}
+}}}
 
 
 
-write_rds(raw_df, "data/generated_data/loocv_df.rds")
+write_rds(raw_df, "data/generated_data/analysis_objects/loocv_df.rds")
+
+
+# #get fit data
+# bangladeshi_fitdata <- final_wide %>%
+#         filter(cohort %in% c("BGD Vaccinee" , "SMIC/PIC"))  
+# missing <-bangladeshi_fitdata[,var_list$`All Variables`] %>% is.na() %>% rowSums()
+# bangladeshi_fitdata <- bangladeshi_fitdata[missing==0,]
+# 
+# 
+# tree_param <- 1000
+# t <-200
+# ids <- unique(bangladeshi_fitdata$id) 
+# 
+# 
+# loocv_list <- list()
+# raw_df <- data.frame()
+# 
+# for(v in 1:length(var_list)){
+#         for(i in ids){
+# 
+#                 
+#                 marks <- var_list[[v]]
+#                 
+#                 cat("Window: ",t, "Variables: ",names(var_list)[v],"Individiual: ",i,"\n")        
+#                 
+#                 fit_data <- bangladeshi_fitdata %>%
+#                         filter(id!=i) %>%
+#                         addInfectionWindow(end_window=t) 
+#                 
+#                 outcome <- paste0("inf_",t)
+#                 
+#                 # 1. run 100 models to find a cutoff
+#                 #keep 50% fit and 50% to test 
+#                 cut_data <- data.frame()
+#                 
+#                 for(j in 1:100){
+#                         
+#                         n_ind <-unique(fit_data$id) %>% length() 
+#                         
+#                         #define who is in and who is out
+#                         outside <-sample(fit_data$id,round(n_ind*0.5),replace=FALSE)
+#                         outside_df <- filter(fit_data,id %in% outside)
+#                         inside_df  <- filter(fit_data,!id %in% outside)%>%
+#                                 addInfectionWindow(end_window=t)
+#                         
+#                         
+#                         inside_w = NULL
+#                         
+#                         # if(weighted){
+#                         # inside_w <- inside_df %>%
+#                         #         getWeight(end_window = t)  %>%
+#                         #         pull(weight)
+#                         
+#                         # }
+#                         
+#                         #fit rf model
+#                         inside_fit <- ranger::ranger(formula=make_formula(paste0("inf_",t),
+#                                                                           var_list[[v]]),
+#                                                      data=inside_df,
+#                                                      num.trees = 500,
+#                                                      case.weights = inside_w,
+#                                                      replace=TRUE
+#                         )
+#                         
+#                         
+#                         #find the cutoff for each 
+#                         performance <- 
+#                                 outside_df %>% addInfectionWindow(end_window=t) %>%
+#                                 getPerf_ranger(inside_fit,paste0("inf_",t)#,
+#                                                # spec=train_spec_cutoff
+#                                 )
+#                         cut <- distinct(performance$data,youden_cutoff,
+#                                         spec99_cutoff,
+#                                         spec95_cutoff,
+#                                         spec90_cutoff
+#                         )
+#                         cut_data <- bind_rows(cut_data,cut)
+#                 }
+#                 
+#                 #find cutoffs
+#                 median_cut <- cut_data %>%
+#                         summarize(n =n(),
+#                                   youden_cutoff=median(youden_cutoff),
+#                                   spec99_cutoff=median(spec99_cutoff),
+#                                   spec95_cutoff=median(spec95_cutoff),
+#                                   spec90_cutoff=median(spec90_cutoff)
+#                         )
+#                 
+#                 print(median_cut)
+#                 
+#                 # 2. fit the model, predict, and apply cutoff 
+#                 #fit the model
+#                 
+#                 #set weights if needed
+#                 
+#                 w <- NULL
+#                 
+#                 # w <- fit_data%>%
+#                 #         getWeight(end_window = t)  %>%
+#                 #         pull(weight)
+#                 
+#                 
+#                 
+#                 this_fit<- ranger::ranger(data=fit_data,
+#                                           formula=make_formula(outcome,var_list[[v]]),
+#                                           num.trees= tree_param,
+#                                           case.weights = w,
+#                                           replace = TRUE
+#                 )
+#                 
+#                 #
+#                 #calculate the individual's samples seropositivity
+#                 # test_data <- vax_wide_RAU %>%
+#                 #         addInfectionWindow(end_window=t)
+#                 test_data <- bangladeshi_fitdata %>%
+#                         filter(id==i) %>%
+#                         addInfectionWindow(end_window=t)
+#                 
+#                 test_missing <- rowSums(is.na(test_data[var_list[[v]]])) >0
+#                 
+#                 test_data <- test_data[!test_missing,]  
+#                 
+#                 
+#                 test_data["truth"] <- test_data[outcome]
+#                 out_preds <-predict(this_fit,
+#                                     test_data,
+#                                     predict.all=TRUE)
+#                 
+#                 #store the data
+#                 fpr_df <- test_data %>%
+#                         mutate(prediction=out_preds[[1]] %>% rowMeans()-1) %>%
+#                         mutate(youden_cutoff=median_cut$youden_cutoff,
+#                                spec99_cutoff=median_cut$spec99_cutoff,
+#                                spec95_cutoff=median_cut$spec95_cutoff,
+#                                spec90_cutoff=median_cut$spec90_cutoff
+#                         ) %>%
+#                         mutate(
+#                                 youden_seropos = ifelse(prediction>youden_cutoff,
+#                                                         1,0
+#                                 ),
+#                                 spec99_seropos = ifelse(prediction>spec99_cutoff,
+#                                                         1,0),
+#                                 spec95_seropos = ifelse(prediction>spec95_cutoff,
+#                                                         1,0),
+#                                 spec90_seropos = ifelse(prediction>spec90_cutoff,
+#                                                         1,0),
+#                                 
+#                         ) %>%
+#                         mutate(variables=names(var_list)[v]) %>%
+#                         mutate(end_window=as.character(t))
+#                 
+#                 out_tmp <- list(
+#                         cutoffs=cut_data,
+#                         train=fit_data,
+#                         fit=this_fit,
+#                         preds=fpr_df
+#                 )
+# 
+#                 loocv_list[[names(var_list)[v]]][[i]] <- out_tmp
+#                 raw_df <- bind_rows(raw_df,fpr_df)
+#                 
+#                 
+#                 
+#                 
+#                 
+# }}
+# 
+# 
+# 
+# write_rds(raw_df, "data/generated_data/loocv_df.rds")
 
 
 
